c04114b7b07605c162fb4aef66056d98d993e88e
Minju25Kim <48757517+minju25kim@users.noreply.github.com>
2023-01-24 00:02:34 +0000

    setup

---
+++ src/components/calculator.js
@@ -0,0 +1,12 @@
+function Calculator() {
+
+    return(
+      <>
+      <div>this is calculator input.</div>
+      <div>this is calculator output.</div>
+      </>
+
+    )
+}
+
+export default Calculator
